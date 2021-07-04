bcadebc2fcfc62c0e0ed8fc795fae259e755666d
Impermanance <1327426424@qq.com>
2021-07-04 19:06:51 +0800

    httpServer01-bugfix

--- 02nio/src/main/java/jvm/homework/HttpServer01.java
+++ 02nio/src/main/java/jvm/homework/HttpServer01.java
@@ -24,7 +24,7 @@ public class HttpServer01 {
             printWriter.println("HTTP/1.1 200 OK");
             printWriter.println("Content-Type:text/html;charset=utf-8");
             String body = "hello,nio1";
-            printWriter.println("Content-Length"+body.getBytes().length);
+            printWriter.println("Content-Length:"+body.getBytes().length);
             printWriter.println();
             printWriter.write(body);
             printWriter.close();
